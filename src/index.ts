198cafd246857ddcacdf241a22d839b7d65b52fe
pmartinsdev <p6220064@brasilprev.com.br>
2023-02-09 17:38:56 -0300

    fix: remove useless importations

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,1 @@
-import { readFileSync } from "fs";
-import { Pem2Jwk } from "./pem2jwk-parser";
-
 export * from "./pem2jwk-parser";
-
-const pemFile = readFileSync("private-key.pem");
-
-Pem2Jwk.fromPrivateToPublic(pemFile).then(console.log).catch(console.error);
